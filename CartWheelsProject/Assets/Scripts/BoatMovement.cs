9785e381aa5aad608a227f4c3b3ec1f42ed9e920
Owen Taylor <superdonut2000@hotmail.com>
2023-08-30 18:22:37 +1000

    TravelMG Completed
    BoatTravelMG re-completed with button to start game in gardens

--- CartWheelsProject/Assets/Scripts/BoatMovement.cs
+++ CartWheelsProject/Assets/Scripts/BoatMovement.cs
@@ -11,7 +11,7 @@ public class BoatMovement : MonoBehaviour
     public GameObject[] obstacles;
     public static int movementspeed = 1;
     public Vector3 userDirection = Vector3.right;
-    public GameObject winText; //lazy option for now
+    public GameObject[] activateOnWin; //lazy option for now
     // Start is called before the first frame update
     void Start()
     {
@@ -27,9 +27,12 @@ public class BoatMovement : MonoBehaviour
     {
         if (collision.gameObject == finishLine)
         {
-            winText.SetActive(true);
-            winText.GetComponent<TextMeshProUGUI>().text = "YOU WIN!!\nYou got " + manager.GetComponent<BoatTravelManager>().lives + " star(s)!";
-            exit.GetComponent<SceneLoader>().LoadScene("EatStreet");
+            foreach (GameObject _object in activateOnWin)
+            {
+                _object.SetActive(true);
+            }
+            activateOnWin[0].GetComponent<TextMeshProUGUI>().text = "YOU WIN!!\nYou got " + manager.GetComponent<BoatTravelManager>().lives + " star(s)!";
+            //exit.GetComponent<SceneLoader>().LoadScene("EatStreet");
 
         }
 
