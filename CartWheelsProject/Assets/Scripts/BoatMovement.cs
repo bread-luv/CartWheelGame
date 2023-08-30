fd0bd1adc20782ab9052dd29b76de863f927137a
RileyGue <104498669+RileyGue@users.noreply.github.com>
2023-08-30 16:26:00 +1000

    Assets

--- CartWheelsProject/Assets/Scripts/BoatMovement.cs
+++ CartWheelsProject/Assets/Scripts/BoatMovement.cs
@@ -1,20 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BoatMovement : MonoBehaviour
 {
+    public GameObject exit;
+    public GameObject finishLine;
+    public GameObject manager;
+    public GameObject[] obstacles;
     public static int movementspeed = 1;
     public Vector3 userDirection = Vector3.right;
+    public GameObject winText; //lazy option for now
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(userDirection * movementspeed * Time.deltaTime);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == finishLine)
+        {
+            winText.SetActive(true);
+            winText.GetComponent<TextMeshProUGUI>().text = "YOU WIN!!\nYou got " + manager.GetComponent<BoatTravelManager>().lives + " star(s)!";
+            exit.GetComponent<SceneLoader>().LoadScene("EatStreet");
+
+        }
+
+        foreach (GameObject obstacle in obstacles)
+        {
+            if (collision.gameObject == obstacle)
+            {
+                manager.GetComponent<BoatTravelManager>().lives -= 1;
+
+            }
+        }
     }
 }
---
+++ CartWheelsProject/Assets/Scripts/BotanicGardens/BoatTravelMinigame/BoatTravelManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class BoatTravelManager : MonoBehaviour
+{
+    public int lives = 3;
+    public GameObject livesText;
+    public GameObject boat;
+    public GameObject failtext;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(lives);
+        livesText.GetComponent<TextMeshProUGUI>().text = lives.ToString();
+        if (lives <= 0)
+        {
+            boat.SetActive(false);
+            failtext.SetActive(true);
+        }
+    }
+}
