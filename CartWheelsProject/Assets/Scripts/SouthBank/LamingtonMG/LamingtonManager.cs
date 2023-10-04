70a0f8d66345cf49e21c8a23263e78f951da0c6f
RileyGue <104498669+RileyGue@users.noreply.github.com>
2023-10-04 17:39:18 +1000

    Complete

--- CartWheelsProject/Assets/Scripts/SouthBank/LamingtonMG/LamingtonManager.cs
+++ CartWheelsProject/Assets/Scripts/SouthBank/LamingtonMG/LamingtonManager.cs
@@ -8,15 +8,13 @@ public class LamingtonManager : MonoBehaviour
 {
     public static int mashCounter = 0;
     public GameObject[] Ingredients;
+    public GameObject[] Ingredients2;
+    public GameObject[] Ingredients3;
     public GameObject Debugtext;
+    public GameObject Debugtext2;
     public GameObject MashButton;
     private int phase = 1;
-    //private bool phasecomplete = false;
-    // Start is called before the first frame update
-    //public void MashCounter(string yes)
-    //{
-        //mashCounter++;
-    //}
+    
     void Start()
     {
         
@@ -30,37 +28,117 @@ public class LamingtonManager : MonoBehaviour
             phase = 2;
             for (int i = 0; i < Ingredients.Length; i++)
             {
-             
+
                 if (Ingredients[i].activeSelf)
                 {
-                    Debugtext.GetComponent<TextMeshProUGUI>().text = "no phase 2";
+                    Debugtext.GetComponent<TextMeshProUGUI>().text = "Add the Ingredients to the Bowl!";
                     phase = 1;
                     break;
                 }
-                
+
             }
         }
+
         if (phase == 2)
-        {   
-            Debugtext.GetComponent<TextMeshProUGUI>().text = "no phase 3";
+        {
+            Debugtext.GetComponent<TextMeshProUGUI>().text = "Hit the Button to mix the ingredients!";
             MashButton.SetActive(true);
+            Debugtext2.SetActive(true);
             phase = 3;
             if (mashCounter < 10)
             {
                 phase = 2;
-                Debugtext.GetComponent<TextMeshProUGUI>().text = "phase 3";
-               
             }
             else
             {
-                phase = 3;
+                phase = 10;
+                mashCounter = 0;
             }
-            
+
         }
+
+        if (phase == 10)
+        {
+            for (int i = 0; i < Ingredients2.Length; i++)
+            {
+                Ingredients2[i].SetActive(true);
+                
+            }
+            phase = 3;
+        }
+
         if (phase == 3)
         {
-            Debugtext.GetComponent<TextMeshProUGUI>().text = "phase 3 confirmed";
+            Debugtext.GetComponent<TextMeshProUGUI>().text = "Add Icing and the Lamingtons to the Bowl!";
             MashButton.SetActive(false);
+            Debugtext2.SetActive(false);
+            phase = 4;
+            for (int i = 0; i < Ingredients2.Length; i++)
+            {
+
+                if (Ingredients2[i].activeSelf)
+                {
+                    
+                    phase = 3;
+                    break;
+                }
+
+            }
+
+        }
+        if (phase == 4)
+        {
+            Debugtext.GetComponent<TextMeshProUGUI>().text = "Hit the Button to add the Icing to the Lamingtons!";
+            MashButton.SetActive(true);
+            Debugtext2.SetActive(true);
+            Debugtext2.GetComponent<TextMeshProUGUI>().text = "" + mashCounter;
+            phase = 5;
+
+            if (mashCounter < 10)
+            {
+                phase = 4;
+                
+
+            }
+            else
+            {
+                phase = 11;
+                mashCounter = 0;
+            }
+
+        }
+        if (phase == 11)
+        {
+            for (int i = 0; i < Ingredients3.Length; i++)
+            {
+                Ingredients3[i].SetActive(true);
+            }
+            phase = 5;
+            
+        }
+
+        if (phase == 5)
+        {
+            Debugtext2.SetActive(false);
+            MashButton.SetActive(false);
+            Debugtext.GetComponent<TextMeshProUGUI>().text = "Add the Lamingtons to the Bowl of Coconut!";
+            phase = 6;
+            for (int i = 0; i < Ingredients3.Length; i++)
+            {
+
+                if (Ingredients3[i].activeSelf)
+                {
+                    
+                    phase = 5;
+                    break;
+                }
+
+            }
+
+        }
+        if (phase == 6)
+        {
+            //Winning stuff happens
         }
     }
 }
--- CartWheelsProject/Assets/Scripts/SouthBank/LamingtonMG/MashButtonHandler.cs
+++ CartWheelsProject/Assets/Scripts/SouthBank/LamingtonMG/MashButtonHandler.cs
@@ -6,17 +6,10 @@ using TMPro;
 
 public class MashButtonHandler : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI txtDisplayNumber;
     public GameObject Debugtext2;
-    
-    
     public void MashCounter()
     {
         LamingtonManager.mashCounter++;
         Debugtext2.GetComponent<TextMeshProUGUI>().text = "" + LamingtonManager.mashCounter;
-    }
-    public void DisplayCounter()
-    {
-
     }
 }
