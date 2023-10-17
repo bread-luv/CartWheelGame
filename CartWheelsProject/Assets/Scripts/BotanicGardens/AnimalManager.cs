504104ecd2b7fab4dc282894e2ffcf907dd59400
Owen Taylor <superdonut2000@hotmail.com>
2023-10-17 21:16:20 +1000

    Fully functional
    All good

--- CartWheelsProject/Assets/Scripts/BotanicGardens/AnimalManager.cs
+++ CartWheelsProject/Assets/Scripts/BotanicGardens/AnimalManager.cs
@@ -17,14 +17,12 @@ public class AnimalManager : MonoBehaviour
 
     [SerializeField]
     public static int totalAnimals = 0;
-    //public static int totalAnimals = 1;
     private int Randint;
 
     [SerializeField]
-    public static int totalCorrectAnimals = 1;
-    //public int totalCorrectAnimals = 0;
+    public static int totalCorrectAnimals = 3;
 
-    public int maxScore = 1;
+    public int maxScore = 3;
     public GameObject scoreLabel;
 
     public GameObject Score;
@@ -36,21 +34,29 @@ public class AnimalManager : MonoBehaviour
     public AudioClip loseSound;
     private bool soundPlayed;
 
+    private int old_score;
+    private int currentAnimalVal = 0;
+
+    public AudioSource sound_source;
+    public AudioClip correct_sound;
+    public AudioClip incorrect_sound;
 
     // Start is called before the first frame update
     void Start()
     {
-        Randint = Random.Range(0, animalStorage.Length);
-
-        currentAnimals = animalStorage[Randint].gameObject;
-
+        animalStorage = ShuffleArray(animalStorage);
+        randomAnimal(0);
+    }
+    void randomAnimal(int _i)
+    {
+        //Randint = Random.Range(0, animalStorage.Length);
+        currentAnimals = animalStorage[_i].gameObject;
         totalAnimals = currentAnimals.transform.childCount;
-
         Animals = new GameObject[totalAnimals];
 
         for (int i = 0; i < Animals.Length; i++)
         {
-            Animals[i] = animalStorage[Randint].transform.GetChild(i).gameObject;
+            Animals[i] = animalStorage[_i].transform.GetChild(i).gameObject;
         }
 
         currentAnimals.gameObject.SetActive(true);
@@ -58,17 +64,16 @@ public class AnimalManager : MonoBehaviour
 
     private void Update()
     {
-        //for (j = 0; j < 3; j++)
-        //{
-            //if (scoreLabel.GetComponent<ScoreScript>().currentScore == maxScore && j < 3)
-            //{
-                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                //break;
-            //}
-            if (scoreLabel.GetComponent<ScoreScript>().currentScore == maxScore)
+        if (currentAnimalVal == maxScore)
+        {
+            if (scoreLabel.GetComponent<ScoreScript>().currentScore == 0)
+            {
+                Lose();
+            }
+            else
             {
                 winText.SetActive(true);
-                stars = 3;
+                stars = scoreLabel.GetComponent<ScoreScript>().currentScore;
                 _winText.GetComponent<Text>().text = "YOU WIN!\n\n\nYou got " + scoreLabel.GetComponent<ScoreScript>().currentScore + " animals correct!\n You earned " + stars + " star(s)!\nYou earned $" + money[stars - 1] + "!";
 
                 if (!moneyAdded && stars > 0)
@@ -83,14 +88,26 @@ public class AnimalManager : MonoBehaviour
                 }
                 _Animals.SetActive(false);
             }
-            else if (scoreLabel.GetComponent<ScoreScript>().currentScore < 0)
-            {
-                Incorrect();
-            }
-        //}
+        }
+
+        if (scoreLabel.GetComponent<ScoreScript>().currentScore < old_score)
+        {
+            sound_source.PlayOneShot(incorrect_sound);
+            scoreLabel.GetComponent<ScoreScript>().currentScore += 1;
+            currentAnimalVal += 1;
+            Incorrect();
+            old_score = scoreLabel.GetComponent<ScoreScript>().currentScore;
+        }
+        else if (scoreLabel.GetComponent<ScoreScript>().currentScore > old_score)
+        {
+            sound_source.PlayOneShot(correct_sound);
+            currentAnimalVal += 1;
+            Incorrect();
+            old_score = scoreLabel.GetComponent<ScoreScript>().currentScore;
+        }
     }
 
-    public void Incorrect()
+    public void Lose()
     {
         winText.SetActive(true);
         _winText.GetComponent<Text>().text = "YOU LOSE!\n\n\nYou got 0 animals correct!\n You earned 0 stars!\nYou earned $0!";
@@ -102,9 +119,26 @@ public class AnimalManager : MonoBehaviour
         _Animals.SetActive(false);
     }
 
+    void Incorrect()
+    {
+        currentAnimals.gameObject.SetActive(false);
+        randomAnimal(currentAnimalVal);
+    }
+
     public static void CorrectPosition()
     {
         totalCorrectAnimals += 1;
+    }
 
+    GameObject[] ShuffleArray(GameObject[] array)
+    {
+        for (int i = array.Length; i > 0; i--)
+        {
+            int j = Random.Range(0, i);
+            GameObject k = array[j];
+            array[j] = array[i - 1];
+            array[i - 1] = k;
+        }
+        return array;
     }
 }
